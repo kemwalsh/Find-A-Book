54c76426644770bc11894901ee58817a6bc6c3ee
Kelly Walsh <kellywalsh827@gmail.com>
2022-08-01 01:43:21 -0600

    define typeDefs and resolvers in server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
